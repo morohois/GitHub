84404ddb5e4629594e551928f379bf292b452684
Daniel Franken <33347130+morohois@users.noreply.github.com>
2018-04-17 18:03:44 +0200

    Habs leer gemacht
    ist jetzt leer

--- hallo.js
+++
@@ -1,5 +0,0 @@
-console.log("Du bist ein Affe!");
-
-//fj
-console.log("GitHub geht mir auf den Sack!");
-console.log("Jo das ist dann die Dritte Zeile!!!");
--- test123.js
+++
@@ -1,9 +0,0 @@
-console.log("Ich verzweifle!");
-
-console.log("Irgendwas klappt hier nicht 17.46");
-
-
-console.log(1+2+6);
-
-console.log("hahahahahahahahahaha")
-console.log("Ich hoffe das funktioniert jetzt");
