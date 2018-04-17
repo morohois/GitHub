4c3acab8e9d9beb6d229e667e8489ae4e3b36ced
Benedict Weinbach <32922900+TheScreamy@users.noreply.github.com>
2018-04-17 18:27:22 +0200

    jo

--- Wichtig.js
+++ Wichtig.js
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 console.log("Funktionen");
 
 console.log("Dani war hier");
 console.log("Bene war hier!");
 console.log("Lösche alles");
+=======
+>>>>>>> 1e2375db06bc8baab03c424e54e0a356d44abb09
