f66433e85f6b497ea75d31ab85ad044cdbf06085
Benedict Weinbach <32922900+TheScreamy@users.noreply.github.com>
2018-04-17 16:50:49 +0200

    ha

--- test123.js
+++ test123.js
@@ -1,5 +1,4 @@
 console.log("hello World!");
 console.log("haha");
 console.log("Aber du auch");
-
 console.log("2 * 3 macht 4 widdewiddewid und 3 macht neune"); console.log("hahah das funktioniert eh nicht");
